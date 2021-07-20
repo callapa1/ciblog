3872996d091e64ebf3845793675b568d93ffe5dd
callapa1 <54779545+callapa1@users.noreply.github.com>
2021-07-20 17:56:41 +0200

    post images are shown on posts/index

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,7 +2,14 @@
 
 <?php foreach($posts as $post): ?>
     <h3><?= $post['title']; ?></h3>
-    <small class="post-date">Posted on <?= $post['created_at']; ?> in <strong><?= $post['name']; ?></strong></small>
-    <p><?= word_limiter($post['body'], 60); ?></p>
-    <p><a class="btn btn-info" href="<?= site_url('/posts/'.$post['slug']); ?>">Read more</a></p>
+    <div class="row">
+        <div class="col-md-3">
+            <img class="post-thumb" src="<?= site_url(); ?>assets/images/posts/<?= $post['post_image']; ?>">
+        </div>
+        <div class="col-md-9">
+            <small class="post-date">Posted on <?= $post['created_at']; ?> in <strong><?= $post['name']; ?></strong></small>
+            <p><?= word_limiter($post['body'], 60); ?></p>
+            <p><a class="btn btn-info" href="<?= site_url('/posts/'.$post['slug']); ?>">Read more</a></p>
+        </div>
+    </div>
 <?php endforeach;?>
