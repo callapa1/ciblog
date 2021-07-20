14c9729a6597c8c3f6de98ea06573dd202c6ecdc
callapa1 <54779545+callapa1@users.noreply.github.com>
2021-07-20 17:19:12 +0200

    Categories created for posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,7 +2,7 @@
 
 <?php foreach($posts as $post): ?>
     <h3><?= $post['title']; ?></h3>
-    <small class="post-date">Posted on <?= $post['created_at'] ?></small>
-    <p><?= $post['body']; ?></p>
+    <small class="post-date">Posted on <?= $post['created_at']; ?></small>
+    <p><?= word_limiter($post['body'], 60); ?></p>
     <p><a class="btn btn-info" href="<?= site_url('/posts/'.$post['slug']); ?>">Read more</a></p>
 <?php endforeach;?>
