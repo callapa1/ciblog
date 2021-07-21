efa61127bae3d055a380806ad54edd999b7f7fb7
callapa1 <54779545+callapa1@users.noreply.github.com>
2021-07-21 17:27:28 +0200

    get_categories method created in Categories_model

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -1,7 +1,12 @@
 <?php
 class Categories extends CI_Controller{
     public function index(){
+        $data['title'] = 'Categories';
+        $data['categories'] = $this->category_model->get_categories();
         
+        $this->load->view('templates/header');
+        $this->load->view('categories/index', $data);
+        $this->load->view('templates/footer');
     }
 
     public function create(){
