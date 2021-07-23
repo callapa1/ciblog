d743cb7afd1d7aca502b5a67bdce6a0b1564e0b6
callapa1 <54779545+callapa1@users.noreply.github.com>
2021-07-23 14:44:41 +0200

    User_model with register function created. Users can be created succesfully

---
+++ application/models/User_model.php
@@ -0,0 +1,16 @@
+<?php
+class User_model extends CI_Model{
+    public function register($enc_password){
+        // User data array
+        $data = array(
+            'name' => $this->input->post('name'),
+            'email' => $this->input->post('email'),
+            'username' => $this->input->post('username'),
+            'password' => $enc_password,
+            'zipcode' => $this->input->post('zipcode')
+        );
+
+        // Insert
+        return $this->db->insert('users', $data);
+    }
+}
